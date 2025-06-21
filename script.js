de8859af5aa920ae7e8bf86874f91d64cc477403
FridasamKarimi <muriithifridasam@gmail.com>
2025-06-21 04:22:24 +0300

    new changes

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+/*
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number') = 13;
+document.querySelector('.score').textContent = 16;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value;
+*/
+const x = function() {
+    console.log(34);
+}
+
+document.querySelector('.check').addEventListener
+('click', function() {
+   const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = "No number!"
+    }
+
+});
